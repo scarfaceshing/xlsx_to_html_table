ff71f60c16d6d986d6b61b84cd926f7f3b0900d0
edrianjansolatorio <edrian.solatorio@arbcalls.com>
2022-02-20 00:12:03 +0800

    Update package-lock.json, package.json, and 6 more files...

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+    interface ProcessEnv {
+      PRINT_VIEW: boolean,
+      NODE_ENV: 'development' | 'production' | 'test'
+      PUBLIC_URL: string
+    }
+  }
